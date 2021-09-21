395ddfe57aaa9c65725142a5027c713682f5acbb
Nitesh Padghan <65269248+niteshp55@users.noreply.github.com>
2021-09-21 17:04:12 +0530

    Update contact-us-3.js

--- pages/contact-us-3.js
+++ pages/contact-us-3.js
@@ -7,39 +7,16 @@ function ContactUs1() {
   return (
     <>
       <Header3 />
-      <div className="page-content bg-white">
+      <div className="page-content bg-white"
+       style={{
+        backgroundImage: "url(images/banner/bnr2.png), var(--gradient-sec)",
+        backgroundSize: "cover, 200%",
+      }}
+      >
         {/* <!-- Banner  --> */}
-        <div
-          className="dlab-bnr-inr style-1 bg-primary"
-          style={{
-            backgroundImage: "url(images/banner/bnr2.png), var(--gradient-sec)",
-            backgroundSize: "cover, 200%",
-          }}
-        >
-          <div className="container">
-            <div className="dlab-bnr-inr-entry">
-              <h1>Contact Us</h1>
-              {/* <!-- Breadcrumb Row --> */}
-              <nav aria-label="breadcrumb" className="breadcrumb-row style-1">
-                <ul className="breadcrumb">
-                  <li className="breadcrumb-item">
-                    <Link href="/">
-                      <a>Home</a>
-                    </Link>
-                  </li>
-                  <li className="breadcrumb-item active" aria-current="page">
-                    Contact Us
-                  </li>
-                </ul>
-              </nav>
-              {/* <!-- Breadcrumb Row End --> */}
-            </div>
-          </div>
-        </div>
-        {/* <!-- Banner End --> */}
-
-        {/* <!-- Get A Quote --> */}
         <Quote3/>
+        
+        
       </div>
       <Footer3 />
     </>
