2340978ddca6643f9a76818419ee4a516d13fbdd
Nitesh Padghan <65269248+niteshp55@users.noreply.github.com>
2021-11-09 12:16:08 +0530

    2 blogs deleted

--- element/blog-3.js
+++ element/blog-3.js
@@ -65,60 +65,7 @@ function Blog3() {
 							</div>
 						</div>
 					</div>
-					<div className="col-xl-6 wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
-						<div className="dlab-blog blog-half m-b30">
-							<div className="dlab-media">
-								<img src="images/blog/default/blog3.jpg" alt=""/>
-							</div>
-							<div className="dlab-info">
-								<h5 className="dlab-title">
-									<Link href="blog-details-3"><a>How to set up your Bluehost account in just 6 easy steps
-</a></Link>
-								</h5>
-								<p>Want to start your website but don’t know how to handle all the technical stuff. Well, leave that on Bluehost. </p>
-								<div className="dlab-meta">
-									<ul>
-										<li className="post-date"><i className="flaticon-clock m-r10"></i>19 Sept, 2021</li>
-										<li className="post-comment"><a href="javascript:void(0);"><i className="flaticon-speech-bubble"></i><span>15</span></a></li>
-										<li className="post-share"><i className="flaticon-share"></i>
-											<ul>
-												<li><a className="fa fa-facebook" href="javascript:void(0);"></a></li>
-												<li><a className="fa fa-twitter" href="javascript:void(0);"></a></li>
-												<li><a className="fa fa-linkedin" href="javascript:void(0);"></a></li>
-											</ul>
-										</li>
-									</ul>
-								</div>								
-							</div>
-						</div>
-					</div>
-					<div className="col-xl-6 wow fadeInUp" data-wow-duration="2s" data-wow-delay="0.2s">
-						<div className="dlab-blog blog-half m-b30">
-							<div className="dlab-media">
-								<img src="images/blog/default/blog4.jpg" alt=""/>
-							</div>
-							<div className="dlab-info">
-								<h5 className="dlab-title">
-									<Link href="blog-details-3"><a>How to set up your Bluehost account in just 6 easy steps
-</a></Link>
-								</h5>
-								<p>Want to start your website but don’t know how to handle all the technical stuff. Well, leave that on Bluehost. </p>
-								<div className="dlab-meta">
-									<ul>
-										<li className="post-date"><i className="flaticon-clock m-r10"></i>19 Sept, 2021</li>
-										<li className="post-comment"><a href="javascript:void(0);"><i className="flaticon-speech-bubble"></i><span>15</span></a></li>
-										<li className="post-share"><i className="flaticon-share"></i>
-											<ul>
-												<li><a className="fa fa-facebook" href="javascript:void(0);"></a></li>
-												<li><a className="fa fa-twitter" href="javascript:void(0);"></a></li>
-												<li><a className="fa fa-linkedin" href="javascript:void(0);"></a></li>
-											</ul>
-										</li>
-									</ul>
-								</div>								
-							</div>
-						</div>
-					</div>
+					
 				</div>
 			</div>
 		</section>
