36631178c55c042e0c932e8d39e764ec474831ea
Nitesh Padghan <65269248+niteshp55@users.noreply.github.com>
2021-11-12 21:19:22 +0530

    faq uploaded

--- element/accordion_sm.js
+++ element/accordion_sm.js
@@ -5,40 +5,64 @@ function Accordion_sm() {
     const [activeDefault, setActiveDefault] = useState(0);
     const defaultAccordion = [
         {
-            title: "Fusce sem ligula, imperdiet sed nisi sit amet ?",
+            title: "How much does it cost to create a website?",
             text:
-                "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.",
+                "Well, our prices depends on so many factors. It would be impossible to say it in one sentence.Avoid any web developer who can answer this question without a long discussion. To get a better idea on the cost of the website you need to build-
+1. Send us your message through our contact form or give us a call.  
+
+2. Answer few questions regarding nature of your website, features, design etc.
+
+3. We’ll provide you a ballpark figure based on our initial discussions.
+
+4. If you are happy, we’ll proceed and come up with the final quote.
+
+Free advice! Avoid any developer who gives you a quote instantly. Giving a quote instantly without proper discussions is Impossible.",
             bg: "primary",
         },
         {
-            title: "Maecenas aliquet quam sed tellus cursus ?",
+            title: "What If I am not satisfied by your service?",
             text:
-                "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.",
+                "Oh No! We really don’t hope for that. But as per our business agreement and policy, the unsatisfied customer will be paid off within 90 days(It normally takes that time to get our accounts set right and return the funds to you).",
 
             bg: "info",
         },
         {
-            title: "Proin blandit sed arcu sed ultricies ?",
+            title: "What are your payment steps?",
             text:
-                "Donec suscipit porta lorem eget condimentum. Morbi vitae mauris in leo venenatis varius. Aliquam nunc enim, egestas ac dui in, aliquam vulputate erat. Curabitur porttitor ante ut odio vestibulum, et iaculis arcu scelerisque.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.",
+                "We require 50% of the project value at the time of starting the project. After the design is approved and the coding is complete, we’ll show the website from our server. Once the website is approved, you need to pay the balance 50% to enable us to transfer the website to your server. For projects with high value, we are happy to discuss and receive payments in 2 or more installments. Yes, we are here to sort it out. Let’s talk!",
 
             bg: "success",
             
         },
         {
-            title: "Proin cursus massa ipsum, sit amet ?",
+            title: "What is a responsive website development?",
             text:
-                "Donec suscipit porta lorem eget condimentum. Morbi vitae mauris in leo venenatis varius. Aliquam nunc enim, egestas ac dui in, aliquam vulputate erat. Curabitur porttitor ante ut odio vestibulum, et iaculis arcu scelerisque.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.",
+                "Responsive web design or responsive web development is an approach to improve the readability of the website pages across various devices, which includes PC desktops, retina displays, tablet screens and mobile phone screens.",
 
             bg: "success",
         },
         {
-            title: "Quisque sem tortor, accumsan finibus massa ?",
+            title: "What is your response turnaround time?",
             text:
-                "Donec suscipit porta lorem eget condimentum. Morbi vitae mauris in leo venenatis varius. Aliquam nunc enim, egestas ac dui in, aliquam vulputate erat. Curabitur porttitor ante ut odio vestibulum, et iaculis arcu scelerisque.Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.",
+                "We will respond to all the emails quickly. Approximately the first response can be expected within 90 minutes in our working hours. Within 24 hours all the first responses will be executed. ",
 
             bg: "success",
         },
+                {
+            title: "Will I have a say in the graphic design process?",
+            text:
+                "Definitely, Our goal is to satisfy you and your input is very important to us. You can give us any site that you like and we will be happy to emulate and still come up with a site that will be consistent and will represent your whole brand.",
+
+            bg: "success",
+        },
+                        {
+            title: "What if I need help on my site down the road?",
+            text:
+                "We are only an email away! We’re here to help you as much or as little as you need, and we won’t disappear once the site is launched. We’ve been doing this for many years, so we aren’t going anywhere anytime soon.",
+
+            bg: "success",
+        },
+        
         
     ];
   return (
