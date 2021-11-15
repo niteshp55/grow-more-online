bc7090e457128e7a9de6559d3cc0e750d5260a75
Nitesh Padghan <65269248+niteshp55@users.noreply.github.com>
2021-11-15 11:48:13 +0530

    Update strategy-research.js

--- pages/strategy-research.js
+++ pages/strategy-research.js
@@ -67,19 +67,28 @@ function StrategyResearch() {
 						<div className="dlab-content">
 							<div className="m-b40">
 								<h3>Strategy & Research</h3>
-								<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+								<p>When trying to design a great website it is very easy to focus on making something aesthetically pleasing while forgetting to make sure that the website also achieves your business’ online goals.</p>
 							</div>
 							<div className="row">
 								<div className="col-lg-12">
-									<h3>How You Start Web Development ?</h3>
-									<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley.</p>
+									
+									<p>This is why a well thought-out marketing strategy is crucial to the success of any website design project. Regardless of brand or industry, you need to have a strategy in place so that you are able to identify your business’ objectives and determine what tactics you will need in order to accomplish them. The process of building out an efficient marketing strategy can be complex and should be examined in three phases prior to a website’s launch:</p>
 									<ul className="list-check style-1 primary">
-										<li>First Step</li>
-										<li>Lorem Ipsum has been the industry's standard dummy text ever.</li>
-										<li>when an unknown printer took a galley of type and scrambled.</li>
-										<li>It has survived not only five centuries, but also the leap.</li>
-										<li>Recently with desktop publishing software like Aldus PageMaker.</li>
-										<li>Lorem Ipsum is simply dummy text of the printing industry.</li>
+										<p>How we plan and execute best strategy for your business.</p>
+									<ul className="list-check style-1 primary">
+										<li>The Discovery Phase</li>
+										<p>Developing any strategy starts with extensive research in order to really get a complete understanding of the industry and business itself. A deep analysis of the competitive landscape is imperative to the success of your project, both from a strategy and design perspective. It is crucial to have a mix of both qualitative and quantitative research, such as examining current website metrics, engaging in competitive research, and having conversations with stakeholders will provide you with findings that are going to be relevant throughout the entire strategy process.
+											</p>
+										<p>Conducting your own research with each new initiative gives you supporting quantitative data that will leave you with accurate, unbiased results to eventually use as a basis for decision-making. This information can help plan your web development strategy’s design elements, calls-to-action, navigation, and other items that will increase overall site usability and conversion rate.
+											</p>
+										<li>The Audit Phase</li>
+										<p>The culmination of the discovery phase brings you to the audit—the first real deliverable, that you should be presenting to the decision-makers of your website design project. It’s at this point that you want to take all of your insights from the research you conducted regarding audience, competition, and the current website and use them to present an actionable plan for your website design and pinpoint any goals based on the analysis. For example, if the client is a start-up, comparing them to competitors can lead to completing a branding audit that can verify their positioning and presence in the industry, which will further narrow down the goal of your strategy.</p>
+									
+										<li>The Web Strategy Phase</li>
+											<p>Once your audit is presented, it’s time to move on to the web strategy phase. At this point, you want to focus on taking the insights found through your audit and turning them into legitimate recommendations that integrate various creative and design elements.</p>
+										<li>Effective Processes Drive Achievements</li>
+										<p>While you are developing your web design strategy, every action should be supported and backed up by the specific business objective it will achieve. You want features and content that will constantly keep people coming back. By conducting thorough research, analyzing the insights you find, and applying them to recommended actionable tactics, you can align your business for success and convey a creative branded design for your clients.</p>
+										
 									</ul>
 								</div>
 							</div>
